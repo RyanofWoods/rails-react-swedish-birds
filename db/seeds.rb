9c617d16e0278efa5e6cd113f3df3f891298122f
Ryan Woods <contact@ryanofwoods.com>
2021-04-28 18:56:55 +0200

    create seed file to load up csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# assumes that no models currently exist
+# delete them if you do them already and you want to replace them
+def csv_to_models
+  puts "Creating birds..."
+
+  csv_file_path = Rails.root.join('db', 'data', 'birds.csv')
+  csv_options = { col_sep: ',', headers: :first_row, header_converters: :symbol }
+
+  order = nil
+  family = nil
+
+  CSV.foreach(csv_file_path, csv_options) do |row|
+    unless order&.english_name&.downcase == row[:order_english]&.downcase
+      # we have to create the order
+      order = Order.create!(english_name: row[:order_english].capitalize,
+                            swedish_name: row[:order_swedish].capitalize)
+    end
+
+    unless family&.scientific_name == row[:family_scientific]
+      # we have to create the order
+      family = Family.create!(scientific_name: row[:family_scientific],
+                              english_name: row[:family_english],
+                              swedish_name: row[:family_swedish],
+                              order: order)
+    end
+    
+    Bird.create!(scientific_name: row[:bird_scientific],
+                english_name: row[:bird_english],
+                swedish_name: row[:bird_swedish].titleize,
+                family: family)
+  end
+
+  puts "Added to the database:"
+  puts "- #{Bird.count} birds"
+  puts "- #{Family.count} families"
+  puts "- #{Order.count} orders"
+end  
+
+csv_to_models
