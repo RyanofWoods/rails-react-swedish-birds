4cc4070cfe1c9afd50f785eb52ccab113b3af0bb
Ryan Woods <contact@ryanofwoods.com>
2021-05-02 10:04:22 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :families, only: [:index, :show]
+    end
+  end
 end
