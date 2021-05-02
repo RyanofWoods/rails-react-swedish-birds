b776ece62e4016468b1e3f193b3817199444660c
Ryan Woods <contact@ryanofwoods.com>
2021-05-02 10:23:17 +0200

    remove seen column from birds

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_080754) do
+ActiveRecord::Schema.define(version: 2021_05_02_082136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "birds", force: :cascade do |t|
-    t.boolean "seen", default: false
     t.string "scientific_name"
     t.string "english_name"
     t.string "swedish_name"
