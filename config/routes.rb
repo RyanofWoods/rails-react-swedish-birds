d97f12697122b3649ca8a702555ef5ebfc7945fd
Ryan Woods <contact@ryanofwoods.com>
2021-05-02 11:19:06 +0200

    create observation create method with pundit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :families, only: [:index, :show]
+      resources :birds, only: [] do
+        resources :observations, only: [:create]
+      end
     end
   end
 end
