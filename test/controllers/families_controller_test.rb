b81918f350e924af7d0739c296b6f4fd840d97ce
Ryan Woods <contact@ryanofwoods.com>
2021-05-03 16:12:59 +0200

    add families controller with index, and react and default react component

---
+++ test/controllers/families_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class FamiliesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get families_index_url
+    assert_response :success
+  end
+
+end
