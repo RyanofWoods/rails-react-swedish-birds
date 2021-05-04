a8ed9c5a78d072c6e01c2723b6979824a04a59b5
Ryan Woods <contact@ryanofwoods.com>
2021-05-04 16:00:11 +0200

    update mark seen action to update state according to new observation

--- app/javascript/react_app/reducers/selected_family_reducer.js
+++ app/javascript/react_app/reducers/selected_family_reducer.js
@@ -1,6 +1,17 @@
 import { FETCH_FAMILY, MARK_SEEN } from "../actions";
 
 const selectedFamilyReducer = (state, action) => {
+  const updatedState = (name, seen) => {
+    const stateCopy = { ...state };
+    
+    const birdIndex = stateCopy.birds.findIndex((elem) => elem.scientific_name === name);
+
+    stateCopy.birds[birdIndex].seen = seen;
+    stateCopy.total_seen += 1;
+  
+    return stateCopy;
+  }
+
   if (state === undefined) {
     return {
       birds: [],
@@ -11,7 +22,14 @@ const selectedFamilyReducer = (state, action) => {
     case FETCH_FAMILY:
       return action.payload;
     case MARK_SEEN:
-      return state;
+      if (action.payload.error) {
+        return state;
+      } else {
+        return updatedState(
+          action.payload.scientific_name,
+          action.payload.seen
+        );
+      }
     default:
       return state;
   }
