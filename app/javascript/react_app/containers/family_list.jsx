a9e5b3766d31ce87011378e46b6b47349afacd74
Ryan Woods <contact@ryanofwoods.com>
2021-05-04 14:18:14 +0200

    add progress bar on familyList component for each family li, based on seen/total birds

--- app/javascript/react_app/containers/family_list.jsx
+++ app/javascript/react_app/containers/family_list.jsx
@@ -8,11 +8,39 @@ class FamilyList extends Component {
     this.props.fetchFamilies();
   }
 
-  render_family ({ scientific_name, english_name, swedish_name, total_seen, total_birds }) {
+  getRGB(numerator, denominator) {
+    const deci = numerator / denominator; // percentage as decimal
+    const startColor = [235, 55, 40] // color at 0% - rgb -> red
+    const endColor = [55, 235, 40]  // color at 100% - rgb -> green
+    const colorArray = []
+
+    for (let i = 0; i < 3; i++) {
+      // if they are the same number push immediately
+      if (startColor[i] === endColor[i]) {
+        colorArray.push(startColor[i]);
+      } else {
+        // calculate difference
+        const diff = endColor[i] - startColor[i];
+        
+        // add difference * percent onto lowest number
+        colorArray.push(startColor[i] + (Math.floor(diff * deci)));
+      }
+    }
+
+
+    return `rgb(${colorArray.join(',')})`;
+  }
+
+  render_family({ scientific_name, english_name, swedish_name, total_seen, total_birds }) {
+    const progressStyle = {
+      backgroundColor: this.getRGB(total_seen, total_birds)
+    }
+
     return (
       <a href={`/families/${scientific_name}`} key={scientific_name}>
         <li className="list-group-item">
-          <p className="family-list-item-numbers">
+          <div className="progress-indicator" style={progressStyle} />
+          <p className="family-list-item-numbers pl-1">
             ({total_seen}/{total_birds})
           </p>
           <p>
@@ -29,7 +57,9 @@ class FamilyList extends Component {
     return (
       <div>
         <h1>All {totalFamilies} Swedish families: </h1>
-        <h3 className="mb-3">Birds seen: ({totalSeen}/{totalBirds})</h3>
+        <h3 className="mb-3">
+          Birds seen: ({totalSeen}/{totalBirds})
+        </h3>
 
         <ul className="list-group">
           {families.map((family) => {
