c69da8f643e2857dd28aa4a80c0d7e3d2609770e
Ryan Woods <contact@ryanofwoods.com>
2021-05-05 17:15:49 +0200

    add redirect to homepage if user is not signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
