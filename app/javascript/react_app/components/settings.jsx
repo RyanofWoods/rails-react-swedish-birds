f2f52f6d903d69d6cd553f222d076a92b911c29a
Ryan Woods <contact@ryanofwoods.com>
2021-05-07 16:44:35 +0200

    update settings component to have setting options for confirmation and bird groupby

--- app/javascript/react_app/components/settings.jsx
+++ app/javascript/react_app/components/settings.jsx
@@ -4,24 +4,51 @@ import Modal from './modal';
 class Settings extends Component {
   state = {
     showModal: true,
+    settings: { confirm: true,
+                groupBy: 'order'
+              }
   };
 
   toggleModal = () => {
     this.setState({ showModal: !this.state.showModal });
   };
 
+  settingsChange = (id, value) => {
+    const settingsCpy = this.state.settings;
+    settingsCpy[id] = value;
+  
+    this.setState({ settings: settingsCpy })
+  }
+
+  saveSettings = () => {
+    console.log(this.state)
+  }
+
   render() {
     const modalProps = {
-      title: 'Settings',
-      confirmButtonText: 'Save',
+      title: "Settings",
+      confirmButtonText: "Save",
       close: this.toggleModal,
-      action: () => this.props.markSeen(scientific_name)
+      action: this.saveSettings
     };
+
+    const { confirm, groupBy } = this.state.settings;
 
     return (
       this.state.showModal && (
         <Modal {...modalProps}>
-          <p>Save your settings?</p>
+          <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" defaultChecked={confirm} onChange={(event) => this.settingsChange('confirm', event.target.checked)} />
+            <label className="form-check-label" >
+              Confirmation when marking a bird as seen?
+            </label>
+          </div>
+
+          <label className="mr-2" >Group birds by:</label>
+          <select defaultValue={groupBy} className="custom-select mb-3 mr-sm-2" onChange={(event) => this.settingsChange('groupBy', event.target.value)}>
+            <option value="family">Family</option>
+            <option value="order">Order</option>
+          </select>
         </Modal>  
       )
     );
