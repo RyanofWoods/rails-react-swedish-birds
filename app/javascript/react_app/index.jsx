81579685cd2610710ff39bc2197e5bd88e90a8d7
Ryan Woods <contact@ryanofwoods.com>
2021-05-10 15:13:25 +0200

    remove unused ujs from yarn and tidy index

--- app/javascript/react_app/index.jsx
+++ app/javascript/react_app/index.jsx
@@ -14,6 +14,7 @@ import groupsReducer from './reducers/groups_reducer';
 import selectedGroupReducer from './reducers/selected_group_reducer';
 import settingsReducer from './reducers/settings_reducer';
 
+// modules for loading the app with setting defaults and then user settings
 import SETTING_DEFAULTS from './setting_defaults';
 import { loadSettings } from "./actions";
 
