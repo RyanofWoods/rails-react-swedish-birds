d4a854e7bb8e0518b2f7a8c3cd2262a41f20443d
Ryan Woods <contact@ryanofwoods.com>
2021-05-11 09:03:37 +0200

    add validations on english name

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  validates :scientific_name, :swedish_name, presence: true
-  validates :scientific_name, :swedish_name, uniqueness: true
+  validates :scientific_name, :english_name, :swedish_name, presence: true
+  validates :scientific_name, :english_name, :swedish_name, uniqueness: true
 
   has_many :families
   has_many :birds, through: :families
