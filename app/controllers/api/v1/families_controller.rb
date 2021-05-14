2c0967d3fa2a713e008fb9201b6ff4fa0444c455
Ryan Woods <contact@ryanofwoods.com>
2021-05-14 11:54:46 +0200

    add population category to family show query for .birds and bird count

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -33,8 +33,10 @@ class Api::V1::FamiliesController < Api::V1::BaseController
     @family_scientific_name = @family&.scientific_name
     @family_english_name = @family&.english_name
     @family_swedish_name = @family&.swedish_name
-    @family_birds = @family&.birds
-    @total_birds = @family&.birds.count
+
+    @family_birds = @family&.birds_with_population_higher_or_equal_to(params[:population_category_at_least])
+    @total_birds = @family_birds.count
+  
     @total_seen = authorize current_user.family_birds_seen_count(@family), policy_class: FamilyPolicy
   end
 
--- app/models/family.rb
+++ app/models/family.rb
@@ -4,4 +4,10 @@ class Family < ApplicationRecord
 
   has_many :birds
   belongs_to :order
+
+  def birds_with_population_higher_or_equal_to(population_category)
+    return self.birds unless population_category.is_a? Integer
+
+    self.birds.where('population_category <= ?', population_category)
+  end
 end
