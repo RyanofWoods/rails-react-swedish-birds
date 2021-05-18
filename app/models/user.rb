19eb46bcf3e56065e1a76fb1ac8103f22a54df32
Ryan Woods <contact@ryanofwoods.com>
2021-05-18 14:23:25 +0200

    refactor User model via Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,23 @@ class User < ApplicationRecord
 
   def family_birds_seen_count(family, population_category = nil)
     # this captures all the birds apart from the ones with unknown population (pop_cat 100)
-    population_category = POPULATION_DEFAULT unless population_category
+    population_category ||= POPULATION_DEFAULT
 
-    Observation.joins(:bird).where('birds.family_id = :family and user_id = :user and birds.population_category <= :pop_cat',
-                                  { family: family.id, user: self.id, pop_cat: population_category.to_i }).count
-  end 
-  
+    Observation.joins(:bird).where('birds.family_id = :family and
+                                    user_id = :user and
+                                    birds.population_category <= :pop_cat',
+                                   { family: family.id, user: id, pop_cat: population_category.to_i }).count
+  end
+
   def order_birds_seen_count(order, population_category = nil)
     # this captures all the birds apart from the ones with unknown population (pop_cat 100)
-    population_category = POPULATION_DEFAULT unless population_category
+    population_category ||= POPULATION_DEFAULT
 
-    Observation.joins(bird: :family).where('families.order_id = :order and user_id = :user and birds.population_category <= :pop_cat',
-                                  { order: order.id, user: self.id, pop_cat: population_category.to_i }).count
-  end 
+    Observation.joins(bird: :family).where('families.order_id = :order and
+                                            user_id = :user and
+                                            birds.population_category <= :pop_cat',
+                                           { order: order.id, user: id, pop_cat: population_category.to_i }).count
+  end
 
   def groups_data(groups, population_category = nil)
     groups_class = groups.name.downcase
@@ -35,20 +39,20 @@ class User < ApplicationRecord
     groups = groups.map do |group|
       birds = group.birds_with_population_higher_or_equal_to(population_category)
       group_bird_count = birds.count
-  
+
       # the families/orders that have no birds with the given population range
       if birds.size.zero?
         nil
       else
         if groups_class == 'order'
-          group_seen_count = self.order_birds_seen_count(group, population_category)
-        else 
-          group_seen_count = self.family_birds_seen_count(group, population_category)
+          group_seen_count = order_birds_seen_count(group, population_category)
+        else
+          group_seen_count = family_birds_seen_count(group, population_category)
         end
 
         total_seen += group_seen_count
         total_birds += group_bird_count
-        
+
         {
           scientific_name: group.scientific_name,
           english_name: group.english_name,
@@ -62,7 +66,7 @@ class User < ApplicationRecord
     # get rid of the groups that have no birds
     groups = groups.compact
 
-    group_data = {
+    {
       grouped_by: groups_class,
       population_threshold: population_category || POPULATION_DEFAULT,
       total_groups: groups.count,
@@ -70,8 +74,6 @@ class User < ApplicationRecord
       total_birds: total_birds,
       groups: groups
     }
-
-    group_data
   end
 
   def seen_bird?(bird)
