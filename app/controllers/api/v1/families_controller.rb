a050c493f7466c8c39f39a14057aaa8992cd6df9
Ryan Woods <contact@ryanofwoods.com>
2021-05-18 14:48:56 +0200

    refactor controllers via Rubocop

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::FamiliesController < Api::V1::BaseController
 
     pop_cat = params[:population_category_at_least]
 
-    raw_data = authorize current_user.groups_data(groups, pop_cat), policy_class: FamilyPolicy
+    raw_data = authorize current_user.groups_data(groups, pop_cat),
+                         policy_class: FamilyPolicy
 
     render json: raw_data
   end
@@ -27,8 +28,9 @@ class Api::V1::FamiliesController < Api::V1::BaseController
 
     @family_birds = @family&.birds_with_population_higher_or_equal_to(params[:population_category_at_least])
     @total_birds = @family_birds.count
-  
-    @total_seen = authorize current_user.family_birds_seen_count(@family, params[:population_category_at_least]), policy_class: FamilyPolicy
+
+    @total_seen = authorize current_user.family_birds_seen_count(@family, params[:population_category_at_least]),
+                            policy_class: FamilyPolicy
   end
 
   private
