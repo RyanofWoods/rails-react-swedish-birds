0d8ef6105d8defe772759c2dbb5932b61f68b103
Ryan Woods <contact@ryanofwoods.com>
2021-05-18 14:18:09 +0200

    refactor Observation model via Rubocop

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,5 +1,5 @@
 class Observation < ApplicationRecord
-  validates :bird, uniqueness: [ scope: :user, message: 'User can only add an observation for a bird species once.' ]
+  validates :bird, uniqueness: [scope: :user, message: 'User can only add an observation for a bird species once.']
 
   belongs_to :bird
   belongs_to :user
