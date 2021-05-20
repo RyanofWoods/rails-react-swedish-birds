913ed0225065a66a60109f927b48dc372aba90bc
Ryan Woods <76776099+RyanofWoods@users.noreply.github.com>
2021-05-20 18:19:26 +0200

    Appearance tweaks (#40)
    * fix group li numbers spacing - was broken for big numbers
    
    * moved container to wrapper and wrapper to app component
    
    * fix typo and indentation

--- app/javascript/react_app/components/wrapper.jsx
+++ app/javascript/react_app/components/wrapper.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Wrapper = ({children}) => (
-  <div className="row justify-content-center">
-    <div className="col-12 col-lg-8">
-      {children}
+  <div className="container my-5">
+    <div className="row justify-content-center">
+      <div className="col-12 col-lg-8">
+        {children}
+      </div>
     </div>
   </div>
 );
