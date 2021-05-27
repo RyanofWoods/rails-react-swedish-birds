30e08c4ca8b36ca56cd6fbd5aba42da2faa1a2c3
Ryan Woods <contact@ryanofwoods.com>
2021-05-27 16:47:33 +0200

    make basic details modal

---
+++ app/javascript/react_app/components/details_modal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Modal from './modal';
+
+const DetailsModal = ({ close }) => (
+  <Modal title="Details" close={close}>DetailsModal</Modal>
+);
+
+export default DetailsModal;
