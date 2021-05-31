4abeb56cfd28bcaa50e1b1e78a85214eddabd080
Ryan Woods <contact@ryanofwoods.com>
2021-05-31 16:21:17 +0200

    update reducers to handle new sort functionality on userLangPref: 'both'
    When the user has the language preference setting saved as 'both'
    and toggles the name in the list headers. It will now increment
    both languages. EN>SE>NULL. Before it would just order by
    english_name.
    
    Now the user can toggle through both languages without having to
    change their settings. As well as this, if they want to for
    example order by Swedish to find a bird/group they can
    still keep the English name which allows them to find a bird/group
    if they only know the name in one language

--- app/javascript/react_app/reducers/groups_reducer.js
+++ app/javascript/react_app/reducers/groups_reducer.js
@@ -28,19 +28,11 @@ const groupsReducer = (state, action) => {
     return stateCopy;
   };
 
-  const sortGroups = (groups, sortBy, userLangPref = null) => {
-    if (!sortBy) return groups;
+  const sortGroups = (groups, sortBy) => {
+    if (!sortBy) return [...groups];
 
-    let [key] = Object.keys(sortBy);
+    const [key] = Object.keys(sortBy);
     const order = sortBy[key];
-
-    if (key === 'name') {
-      if (userLangPref === 'se') {
-        key = 'swedish_name';
-      } else {
-        key = 'english_name';
-      }
-    }
 
     const sortedGroups = [...groups].sort((a, b) => {
       // sort by seen percentage
@@ -66,21 +58,49 @@ const groupsReducer = (state, action) => {
   };
 
   const handleSortHeaderClick = ({ clickedHeader, userLangPref }) => {
+    const { sortedBy } = state;
     let newSortedBy = {};
 
-    // if the key exists, increment it null > asc > desc
-    if (state.sortedBy && clickedHeader in state.sortedBy) {
-      if (state.sortedBy[clickedHeader] === 'asc') {
-        newSortedBy[clickedHeader] = 'desc';
+    const newSort = () => {
+      let key;
+
+      if (clickedHeader === 'name') {
+        if (userLangPref === 'se') {
+          key = 'swedish_name';
+        } else {
+          key = 'english_name';
+        }
       } else {
-        newSortedBy = null;
+        key = clickedHeader;
       }
-    } else { // it is null or a different clicked header
-      newSortedBy[clickedHeader] = 'asc';
+      newSortedBy[key] = 'asc';
+    };
+
+    // if the key exists, increment it null > asc > desc
+    if (sortedBy) {
+      if (clickedHeader in sortedBy || (clickedHeader === 'name' && (Object.keys(sortedBy)[0] === 'english_name' || Object.keys(sortedBy)[0] === 'swedish_name'))) {
+        const key = (clickedHeader === 'name') ? Object.keys(sortedBy)[0] : clickedHeader;
+
+        if (sortedBy[key] === 'asc') {
+          newSortedBy[key] = 'desc';
+        } else if (userLangPref === 'both' && key === 'english_name') {
+          // increment through two languages before going back to null
+          // en asc > en desc > se asc > se desc > null
+          newSortedBy.swedish_name = 'asc';
+        } else {
+          newSortedBy = null;
+        }
+      } else {
+        // new header click
+        newSort();
+      }
+    } else {
+      // was null
+      newSort();
     }
 
     return {
-      sortedGroups: sortGroups(state.groups, newSortedBy, userLangPref),
+      sortedGroups: sortGroups(state.groups, newSortedBy),
       sortedBy: newSortedBy,
     };
   };
