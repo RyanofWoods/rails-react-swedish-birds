999570987b3779fe001fba60be786959dd38e569
Ryan Woods <contact@ryanofwoods.com>
2021-06-01 14:15:37 +0200

    add postgresql trigram extension to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_073739) do
+ActiveRecord::Schema.define(version: 2021_06_01_111807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "birds", force: :cascade do |t|
     t.string "scientific_name"
