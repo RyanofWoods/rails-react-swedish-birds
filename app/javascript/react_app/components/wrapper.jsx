b34699d52e907f159a5dd4058c061e54a112787f
Ryan Woods <contact@ryanofwoods.com>
2021-06-03 18:56:28 +0200

    resolve prop types error and whitespace in wrapper

--- app/javascript/react_app/components/wrapper.jsx
+++ app/javascript/react_app/components/wrapper.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const Wrapper = ({children}) => (
+const Wrapper = ({ children }) => (
   <div className="container my-5">
     <div className="row justify-content-center">
       <div className="col-12 col-lg-8">
