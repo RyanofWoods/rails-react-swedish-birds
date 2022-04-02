ddbc7b0da2ad7ea160b316d803b1d6f9dc6e8a42
Ryan Woods <contact@ryanofwoods.com>
2022-04-02 13:57:10 +0200

    Remove group_birds_seen_count methods from User
    These methods were doing a lot of SQL and was only being used in places
    where the same SQL had already been done for the bird result. Instead,
    we can just look at the amount of observations for the already filtered
    birds.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,26 +10,6 @@ class User < ApplicationRecord
 
   POPULATION_DEFAULT = 9
 
-  def family_birds_seen_count(family, population_category = nil)
-    # this captures all the birds apart from the ones with unknown population (pop_cat 100)
-    population_category ||= POPULATION_DEFAULT
-
-    Observation.joins(:bird).where('birds.family_id = :family and
-                                    user_id = :user and
-                                    birds.population_category <= :pop_cat',
-                                   { family: family.id, user: id, pop_cat: population_category.to_i }).count
-  end
-
-  def order_birds_seen_count(order, population_category = nil)
-    # this captures all the birds apart from the ones with unknown population (pop_cat 100)
-    population_category ||= POPULATION_DEFAULT
-
-    Observation.joins(bird: :family).where('families.order_id = :order and
-                                            user_id = :user and
-                                            birds.population_category <= :pop_cat',
-                                           { order: order.id, user: id, pop_cat: population_category.to_i }).count
-  end
-
   def groups_data(groups, population_category = nil)
     population_category ||= POPULATION_DEFAULT
 
@@ -46,7 +26,7 @@ class User < ApplicationRecord
       if group_bird_count.zero?
         nil
       else
-        group_seen_count = get_group_seen_count(group, grouped_by, population_category)
+        group_seen_count = observations.where(bird: birds).size
 
         total_seen += group_seen_count
         total_birds += group_bird_count
@@ -79,16 +59,6 @@ class User < ApplicationRecord
       true
     else
       false
-    end
-  end
-
-  private
-
-  def get_group_seen_count(group, grouped_by, population_category)
-    if grouped_by == 'order'
-      order_birds_seen_count(group, population_category)
-    else
-      family_birds_seen_count(group, population_category)
     end
   end
 end
