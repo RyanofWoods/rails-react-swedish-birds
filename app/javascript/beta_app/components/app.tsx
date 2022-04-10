e4f434524dcc677801c2e4f5b89b20d5d7c0c712
Ryan Woods <contact@ryanofwoods.com>
2022-04-10 13:08:40 +0200

    Fetch birds on creation of Beta App

--- app/javascript/beta_app/components/app.tsx
+++ app/javascript/beta_app/components/app.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { fetchBirds } from '../api'
+import { useAppDispatch } from '../hooks'
 
 import Navbar from './shared/navbar'
 
 const navbarContainer = document.getElementById('navbar-container') as HTMLElement
 
-const App: React.FC = () => (
-  <>
-    {ReactDOM.createPortal(<Navbar />, navbarContainer)}
-    <div>Hello world</div>
-  </>
-)
+const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+  void dispatch(fetchBirds())
+
+  return (
+    <>
+      {ReactDOM.createPortal(<Navbar />, navbarContainer)}
+    </>
+  )
+}
 
 export default App
