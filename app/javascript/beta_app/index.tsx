c0cf83aa627389cf61ad688c4a5b50cd1ac92c9f
Ryan Woods <contact@ryanofwoods.com>
2022-04-10 13:03:55 +0200

    Use redux toolkit configureStore instead of create store

--- app/javascript/beta_app/index.tsx
+++ app/javascript/beta_app/index.tsx
@@ -1,23 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { logger } from 'redux-logger'
-import reduxPromise from 'redux-promise'
 import { BrowserRouter } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 import App from './components/app'
 
-const reducers = combineReducers({
-})
-
-const initialState = {
+const reducer = {
 }
 
-// root, store and middlewares
-const middlewares = composeWithDevTools(applyMiddleware(logger, reduxPromise))
-const store = createStore(reducers, initialState, middlewares)
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+})
 
 const root = document.getElementById('root')
 
