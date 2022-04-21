90d1b821781b1af9362eb0320e6b845e7706ce47
Ryan Woods <contact@ryanofwoods.com>
2022-04-21 11:15:58 +0200

    Add component to filter Birds by Order and Family
    By using the already written updateFilters action, this component will
    call it when either the Order or Family select is changed.
    
    An important part of this component is that when the Order is changed,
    it should also change the families options to only include families of
    that order.
    
    Default values of the Order and Family select dropdowns will ensure that
    the filters are set to null independently, meaning that all birds or all
    birds for an order (if only the order is set) are rendered.

--- app/javascript/beta_app/components/filters/filter_group.tsx
+++ app/javascript/beta_app/components/filters/filter_group.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 import SearchBar from './search_bar'
 import SeenBirdsFilter from './seen_birds_filter'
+import FamilyAndOrderFilter from './family_and_order_filter'
+import { useAppSelector } from '../../hooks'
 
 const FilterGroup: React.FC = () => {
+  const orders = useAppSelector(state => state.birdsData.orders)
+  const families = useAppSelector(state => state.birdsData.families)
+
   return (
     <>
       <SearchBar />
       <SeenBirdsFilter />
+      <FamilyAndOrderFilter orders={orders} families={families} />
     </>
   )
 }
