0194c2350a63a8f503d895a5a7697eda5ee50e37
Ryan Woods <contact@ryanofwoods.com>
2022-04-21 14:09:28 +0200

    Fix select component handleChange function
    This component now returns the value of the now selected option, rather
    than the event. It also returns null when the the select is changed to
    the default value.

--- app/javascript/beta_app/components/shared/select.tsx
+++ app/javascript/beta_app/components/shared/select.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
+const defaultValue = 'default'
+
 interface SelectProps {
   label?: string
   id: string
   ariaLabel: string
   defaultText: string
   options: Array<{ value: string, text: string }>
-  handleChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void
+  handleChange: (value: string | null) => void
 }
 
 const Select: React.FC<SelectProps> = (props) => {
   const {
     defaultText, options, ariaLabel, label, id, handleChange
   } = props
+
+  const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    const value = event.target.value
+    handleChange(value === defaultValue ? null : value)
+  }
 
   return (
     <div className='form-group'>
@@ -22,9 +29,9 @@ const Select: React.FC<SelectProps> = (props) => {
         id={id}
         aria-label={ariaLabel}
         defaultValue={defaultText}
-        onChange={handleChange}
+        onChange={onSelectChange}
       >
-        <option value='default'>{defaultText}</option>
+        <option value={defaultValue}>{defaultText}</option>
         {options.map((option) => <option key={option.value} value={option.value}>{option.text}</option>)}
       </select>
     </div>
