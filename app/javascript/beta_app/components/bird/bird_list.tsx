03669a3803befff6f2d7693c45de06382c8c10fe
Ryan Woods <contact@ryanofwoods.com>
2022-05-13 09:08:29 +0200

    Add BirdListHeader component to BirdList

--- app/javascript/beta_app/components/bird/bird_list.tsx
+++ app/javascript/beta_app/components/bird/bird_list.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 import Bird from './bird'
 
 import { useAppSelector } from '../../hooks'
+import BirdListHeader from './bird_list_header'
 
 const BirdList: React.FC = () => {
   const birds = useAppSelector(state => state.birdsData.sortedBirds)
   const userSettings = useAppSelector(state => state.birdsData.userSettings)
 
   return (
-    <ul className='bird-list'>
-      {
-        birds.map((birdData) => (
-          <Bird key={birdData.scientificName} bird={birdData} userSettings={userSettings} />
-        ))
-      }
-    </ul>
+    <>
+      <BirdListHeader />
+
+      <ul className='bird-list'>
+        {
+          birds.map((birdData) => (
+            <Bird key={birdData.scientificName} bird={birdData} userSettings={userSettings} />
+          ))
+        }
+      </ul>
+    </>
   )
 }
 
