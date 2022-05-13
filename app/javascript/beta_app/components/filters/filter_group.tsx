cec19f2482e50b8fa6fad7f92fe38a64979e282d
Ryan Woods <contact@ryanofwoods.com>
2022-05-13 10:38:50 +0200

    Add reset filters button
    This button will call the resetFilters action that resets the filters
    back to the default values. Because of this, the Birds are re-filtered
    and re-sorted. The Bird and filter components all get re-rendered.
    
    Original code by Sara Lotfi and commits reorganised by Ryan Woods.
    
    Co-authored-by: Sara Lotfi <lotfi.sara@gmail.com>

--- app/javascript/beta_app/components/filters/filter_group.tsx
+++ app/javascript/beta_app/components/filters/filter_group.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import SearchBar from './search_bar'
 import SeenBirdsFilter from './seen_birds_filter'
 import FamilyAndOrderFilter from './family_and_order_filter'
-import { useAppSelector } from '../../hooks'
+import { useAppSelector, useAppDispatch } from '../../hooks'
+import { resetFilters } from '../../features/birdSlice'
 
 const FilterGroup: React.FC = () => {
   const orders = useAppSelector(state => state.birdsData.orders)
@@ -14,11 +15,21 @@ const FilterGroup: React.FC = () => {
   const selectedSeenValue = useAppSelector(state => state.birdsData.filters.seenScope)
   const searchValue = useAppSelector(state => state.birdsData.filters.searchValue)
 
+  const dispatch = useAppDispatch()
+
+  const handleReset = (event: React.MouseEvent<HTMLElement>): void => {
+    event.preventDefault()
+    void dispatch(resetFilters())
+  }
+
   return (
     <>
       <SearchBar searchValue={searchValue} />
       <SeenBirdsFilter selectedValue={selectedSeenValue} />
       <FamilyAndOrderFilter orders={orders} families={families} userSettings={userSettings} selectedOrderOption={selectedOrderOption} selectedFamilyOption={selectedFamilyOption} />
+      <div className='d-flex justify-content-end'>
+        <a id='reset-filter' href='' onClick={handleReset}>Reset filters</a>
+      </div>
     </>
   )
 }
