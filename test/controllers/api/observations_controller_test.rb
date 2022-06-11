945b45305959dc20f8649afb288e24edb69f0990
Ryan Woods <contact@ryanofwoods.com>
2022-06-11 08:09:26 +0200

    Refactor ObservationsControllerTest
    The goal here was to reduce the number of instance variables. Having
    many but similar variables makes the code more difficult to read as the
    reader needs to understand how the variables relate to each other. For
    example, before, the following existed in the setup:
    - @today = Date.today
    - @observed_at = @today.to_s
    
    During one test, both of these variables may be used, one for the
    arrange, and one for the assertion. Making it more difficult to read
    rather than just using one variable.

--- test/controllers/api/observations_controller_test.rb
+++ test/controllers/api/observations_controller_test.rb
@@ -2,15 +2,13 @@ require 'test_helper'
 
 class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
   setup do
-    new_bird = Bird.create(scientific_name: 'Neo', english_name: 'New', swedish_name: 'Ny', population_category: 1, family: families(:tits))
-    @bird_id = new_bird.scientific_name
+    @new_bird = Bird.create(scientific_name: 'Neo', english_name: 'New', swedish_name: 'Ny', population_category: 1, family: families(:tits))
     @user = users(:ryan)
-    @today = Date.today
-    @observed_at = @today.to_s
+    @observed_at = Date.today
   end
 
   test 'POST #create returns an unauthorized response if there is no logged in user' do
-    post api_bird_observations_url(@bird_id)
+    post api_bird_observations_url(@new_bird.scientific_name)
 
     assert_response :unauthorized
     expected = { 'error'=> 'You need to sign in or sign up before continuing.' }
@@ -31,7 +29,7 @@ class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
     sign_in @user
     observed_at = nil
 
-    post api_bird_observations_url(@bird_id, params: { observed_at: nil })
+    post api_bird_observations_url(@new_bird.scientific_name, params: { observed_at: nil })
 
     assert_response :bad_request
     expected = { 'error'=> 'Observed at must be a Date or zero.' }
@@ -43,7 +41,7 @@ class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
     note = 'First observation notes'
 
     assert_difference('@user.observations.count', 1) do
-      post api_bird_observations_url(@bird_id, params: { note: note, observed_at: @observed_at })
+      post api_bird_observations_url(@new_bird.scientific_name, params: { note: note, observed_at: @observed_at.to_s })
     end
     observation = @user.observations.last
     assert_response :success
@@ -58,10 +56,10 @@ class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
       "seen"=>true,
       "observation"=> {
         'note'=>note,
-        'observedAt'=>@observed_at
+        'observedAt'=>@observed_at.to_s
       }
     }
-    assert_equal(@today, observation.observed_at)
+    assert_equal(@observed_at, observation.observed_at)
     assert_equal(expected, json_response)
   end
 
@@ -69,7 +67,7 @@ class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
     sign_in @user
 
     assert_difference('@user.observations.count', 1) do
-      post api_bird_observations_url(@bird_id, params: { observed_at: @observed_at })
+      post api_bird_observations_url(@new_bird.scientific_name, params: { observed_at: @observed_at.to_s })
     end
     assert_response :success
     expected = {
@@ -83,7 +81,7 @@ class Api::ObservationControllerTest < ActionDispatch::IntegrationTest
       "seen"=>true,
       "observation"=> {
         'note'=>nil,
-        'observedAt'=>@observed_at
+        'observedAt'=>@observed_at.to_s
       }
     }
     assert_equal(expected, json_response)
