7e0f0daaf1f47a79d0d27a38be035b08185511d8
Sara Woods <hellosarawoods@gmail.com>
2022-07-08 15:17:23 +0200

    Fix error in console regarding svg in navbar

--- app/javascript/react_app/components/shared/navbar.tsx
+++ app/javascript/react_app/components/shared/navbar.tsx
@@ -8,9 +8,9 @@ const Navbar = (): JSX.Element => (
 
     <span className='d-flex align-items-center navbar-toggler-icon ml-auto hover-pointer hover-opacity' id='dropdownMenuLink' data-toggle='dropdown'>
       <svg width='24' height='22' viewBox='0 0 24 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <line y1='1' x2='24' y2='1' stroke='#70997B' stroke-width='2' />
-        <line x1='6' y1='11' x2='24' y2='11' stroke='#70997B' stroke-width='2' />
-        <line x1='4.56119e-08' y1='21' x2='24' y2='21' stroke='#70997B' stroke-width='2' />
+        <line y1='1' x2='24' y2='1' stroke='#70997B' strokeWidth='2' />
+        <line x1='6' y1='11' x2='24' y2='11' stroke='#70997B' strokeWidth='2' />
+        <line x1='4.56119e-08' y1='21' x2='24' y2='21' stroke='#70997B' strokeWidth='2' />
       </svg>
     </span>
 
