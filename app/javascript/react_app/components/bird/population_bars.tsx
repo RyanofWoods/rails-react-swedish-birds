c2336d2afbd0a88ecb3c0bd6bbe7d5f114ff07e2
Sara Woods <hellosarawoods@gmail.com>
2022-08-19 18:45:13 +0200

    Refactor populationBars component

--- app/javascript/react_app/components/bird/population_bars.tsx
+++ app/javascript/react_app/components/bird/population_bars.tsx
@@ -1,30 +1,23 @@
 import React from 'react'
 
 import { PopulationCategory } from '../../types/birdData'
-import { isBreedingBird } from '../../helpers/population'
+import { isBreedingBird, populationCategoryToLevel } from '../../helpers/population'
 
 interface PopulationBarsProps {
   population: PopulationCategory
 }
 
 const PopulationBars: React.FC<PopulationBarsProps> = ({ population }) => {
-  let populationLevel = population
-  if (!isBreedingBird(population)) populationLevel -= 5
-
-  let color
-  if (isBreedingBird(population)) {
-    color = 'bar-orange'
-  } else {
-    color = 'bar-blue'
-  }
+  const populationLevel = populationCategoryToLevel(population)
+  const color = (isBreedingBird(population)) ? 'bar-orange' : 'bar-blue'
 
   return (
     <div className='population-bars'>
-      <div className={populationLevel <= 5 ? `bar-filled ${color}` : `bar ${color}`} />
-      <div className={populationLevel <= 4 ? `bar-filled ${color}` : `bar ${color}`} />
-      <div className={populationLevel <= 3 ? `bar-filled ${color}` : `bar ${color}`} />
-      <div className={populationLevel <= 2 ? `bar-filled ${color}` : `bar ${color}`} />
-      <div className={populationLevel === 1 ? `bar-filled ${color}` : `bar ${color}`} />
+      {
+        [...Array(5)].map((_, index) => (
+          <div className={populationLevel >= index + 1 ? `bar-filled ${color}` : `bar ${color}`} key={index} />
+        ))
+      }
     </div>
   )
 }
